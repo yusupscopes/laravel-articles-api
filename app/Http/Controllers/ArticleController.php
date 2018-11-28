4e5d0fb217eeecc4912a68c5215db9c9c6f0c56e
Yusup Maulana <yusupmaulana950@gmail.com>
2018-11-28 12:07:37 +0700

    crud api article

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\Article as ArticleResource;
+use App\Http\Requests;
+use App\Article;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // get articles
+        $articles = Article::latest()->paginate(10);
+        // $articles = Article::latest()->get();
+
+        // return collection of articles as a resource
+        return ArticleResource::collection($articles);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $article = new Article();
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->save();
+
+        return new ArticleResource($article);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // Get article
+        $article = Article::findOrFail($id);
+
+        // return single article as a resource
+        return new ArticleResource($article);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $article = Article::findOrFail($id);
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->update();
+
+        return new ArticleResource($article);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // Get article
+        $article = Article::findOrFail($id);
+        $article->delete();
+
+        return 204;
+        // if ($article->delete()) return new ArticleResource($article);
+    }
+}
